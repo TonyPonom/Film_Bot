821d80234a7b7bd4c9eadff594a65fe7ce32eb62
TonyPonom <67237533+TonyPonom@users.noreply.github.com>
2021-10-28 19:20:51 +0300

    Adop
    New

---
+++ FilmBot/DB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace FilmBot
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root; database=filmpoisk");
+
+        private string _sql { get; set; } = "SELECT * FROM `filmdescription`"; 
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public string GetFromBD(string sql)
+        {
+            string res = "";
+
+            MySqlCommand cm = new MySqlCommand(sql,GetConnection());
+            
+            MySqlDataReader reader = cm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                res = reader[0].ToString() + "\n" + reader[1].ToString();
+            }
+
+            reader.Close(); // закрываем read 
+            
+            return res;
+        }
+        
+        public string SelectFromBD(string sql)
+        {
+            string res;
+            _sql = sql;
+            //string sql = $"SELECT `name`,`descript` FROM `filmdescription` WHERE name={film}";
+            this.openConnection();
+            res = GetFromBD(sql);
+            this.closeConnection();
+            return res;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ FilmBot/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace FilmBot
+{
+    class Program
+    {
+        private static string token { get; set; } = "2034602500:AAE_S3cs8E3ZTxglygveyevYGXg3N-ATapg";
+        private static TelegramBotClient client;
+
+        static string FilmName;
+
+        static string answerOnFilm;
+
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+
+        }
+
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if ((msg.Text != null) & (msg.Type == Telegram.Bot.Types.Enums.MessageType.Text))
+            {
+                Console.WriteLine($"Message come: {msg.Text}");
+
+                DB FilmData = new DB();
+
+                string sql;
+
+                switch (msg.Text)
+                {
+                    case "Поиск фильма":
+
+                        var stick = client.SendStickerAsync(
+                        chatId: msg.Chat.Id,
+                        sticker: "https://tlgrm.ru/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/3.webp",
+                        replyToMessageId: msg.MessageId);
+
+                        answerOnFilm = "Напишите название фильма";
+                        await client.SendTextMessageAsync(msg.Chat.Id, answerOnFilm, replyMarkup: GetButtons());
+                        
+                        break;
+
+                    case "Год выпуска":
+
+                        sql = $"SELECT `name`,`year` FROM `filmdescription` WHERE name LIKE '{FilmName}%'";
+                        answerOnFilm = FilmData.SelectFromBD(sql) + " года выпуска";
+                        await client.SendTextMessageAsync(msg.Chat.Id, answerOnFilm, replyMarkup: GetButtons2());
+
+                        break;
+
+                    case "Оценка кинопоиск":
+
+                        sql = $"SELECT `name`,`starskinopoisk` FROM `evaluation` WHERE name LIKE '{FilmName}%'";
+                        answerOnFilm = FilmData.SelectFromBD(sql);
+                        await client.SendTextMessageAsync(msg.Chat.Id, answerOnFilm, replyMarkup: GetButtons2());
+
+                        break;
+
+                    case "Оценка IMDb":
+
+                        sql = $"SELECT `name`,`starsIMDb` FROM `evaluation` WHERE name LIKE '{FilmName}%'";
+                        answerOnFilm = FilmData.SelectFromBD(sql);
+                        await client.SendTextMessageAsync(msg.Chat.Id, answerOnFilm, replyMarkup: GetButtons2());
+
+                        break;
+
+                    default:
+                        try
+                        {
+                            sql = $"SELECT `name`,`descript` FROM `filmdescription` WHERE name LIKE '{msg.Text}%'";
+                            answerOnFilm = FilmData.SelectFromBD(sql);
+                            FilmName = msg.Text;
+                            await client.SendTextMessageAsync(msg.Chat.Id, answerOnFilm, replyMarkup: GetButtons2());
+                        }
+                        catch (Exception)
+                        {
+                            answerOnFilm = "Данного фильма нет в БД или возникла проблема подключения к БД";
+                            await client.SendTextMessageAsync(msg.Chat.Id, answerOnFilm, replyMarkup: GetButtons2());
+                            return;
+                        }
+                        break;
+
+                }
+                
+                
+                
+            }
+        }
+
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "Гнев человеческий" }, new KeyboardButton { Text = "Дюна" } },
+                    new List<KeyboardButton>{new KeyboardButton { Text = "Джентльмены" }, new KeyboardButton { Text = "Майор Гром: Чумной Доктор" } }
+                }
+            };
+        }
+
+        private static IReplyMarkup GetButtons2()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "Поиск фильма" }, new KeyboardButton { Text = "Год выпуска" } },
+                    new List<KeyboardButton>{new KeyboardButton { Text = "Оценка кинопоиск" }, new KeyboardButton { Text = "Оценка IMDb" } }
+                }
+            };
+        }
+    }
+}
+
